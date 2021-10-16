00a7f3470518230107552831e8d319a78936b86f
Filip Walecki <ippilam@gmail.com>
2021-10-16 19:41:53 +0100

    Buying stocks

--- Concept.py
+++ Concept.py
@@ -1,15 +1,43 @@
 from ib_insync import *
+import csv 
+import pandas as pd
+import pandas_datareader as web 
+import datetime as time
 # util.startLoop()  # uncomment this line when in a notebook
 
 ib = IB()
 ib.connect('127.0.0.1', 7497, clientId=1)
 
-contract = Stock('AMD','SMART','USD')
+stocks = []
+with open('good_stocks.csv', 'r') as f:
+            reader = csv.reader(f)
+            lists = list(f)
+            for i in range(len(lists)):
+                stocks.append(lists[i].strip('\n'))
 
-order = LimitOrder('BUY', 5,112.12)
 
-print(order)
 
-ib.placeOrder(contract , order)
+for i in range (len(stocks)):
 
-print(trade)
+        start = time.datetime(2021,10,15)
+        end = time.datetime(2021,10,15)
+
+        data = web.DataReader(stocks[i] , 'yahoo' , start)
+        print(data)
+        price = data['Close'].values
+        print(price)
+        contract = Stock(stocks[i] ,'SMART','USD')
+        print(contract)
+
+        order = LimitOrder('BUY', 1, round(price[1],2) )
+
+        print(order)
+
+        trade = ib.placeOrder(contract , order)
+
+        print(trade)
+ib.run()
+
+        
+
+        
